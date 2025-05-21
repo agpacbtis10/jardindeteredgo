f762191349e8af335ac2a473f6c016c59f3ac01e
agpacbtis10 <105821631+agpacbtis10@users.noreply.github.com>
2025-05-20 21:22:33 -0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,18 @@
+ </br>
+ </br>
+ <form method="get" action="primero1.php">
+ Nombre: <input type="text" name="nombre"></br>
+ Apellido: <input type="text" name="ape"></br>
+ Correo: <input type="text" name="email"></br>
+ Celular: <input type="text" name="cel"></br>
+
+ </br>
+ <input type="submit" value="enviar">
+ </form>
+ <?php
+ $a=$_GET['nombre'];
+ $b=$_GET['ape'];
+ $c=$_GET['email'];
+ $d=$_GET['cel'];
+ ?>
+
